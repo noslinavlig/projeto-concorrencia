57412034c20352315943bf507aebb1458c4fea90
noslinavlig <gill.sbernardino@gmail.com>
2021-03-24 12:01:52 -0300

    Correcoes

--- Teste.java
+++ Teste.java
@@ -1,7 +1,7 @@
 public class Teste {
 	public static void main(String[] args){
-		Guerreiro warrior1 = new Guerreiro("Perseu", 80, 25);
-		Guerreiro warrior2 = new Guerreiro("Medusa",100, 10);
+		Guerreiro warrior1 = new Guerreiro("Perseu", 70, 15);
+		Guerreiro warrior2 = new Guerreiro("Medusa",100, 20);
 		System.out.println("\nInicio:\n" + warrior1.getNome() + "\t\t" + warrior2.getNome() + "\nVida: " + warrior1.getVida() + "\tVida: " + warrior2.getVida() + "\n");
 		boolean fimdaluta = warrior1.ataca(warrior2);;
 		while(!fimdaluta){
