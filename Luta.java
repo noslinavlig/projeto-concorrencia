7005d96a48beb7e352314080ddb94bc78156c5e6
noslinavlig <gill.sbernardino@gmail.com>
2021-03-24 12:51:06 -0300

    luta.inicio()

--- Luta.java
+++ Luta.java
@@ -1,20 +1,19 @@
 public class Luta {
   
-    private Guerreiro w1;
-    private Guerreiro w2;
+   private Guerreiro w1;
+   private Guerreiro w2;
    
-    public Luta(Guerreiro warrior1, Guerreiro warrior2){
-       this.w1 = warrior1;
-       this.w2 = warrior2;
-    }
-    
-    public void batalha(){
-           System.out.println("\nInicio:\n" + w1.getNome() + "\t\t" + w2.getNome() + "\nVida: " + w1.getVida() + "\tVida: " + w2.getVida() + "\n");
-         
-          do{
+   public Luta(Guerreiro warrior1, Guerreiro warrior2){
+      this.w1 = warrior1;
+      this.w2 = warrior2;
+   }
+
+   public void inicio(){
+      System.out.println("\nInicio:\n" + w1.getNome() + "\t\t" + w2.getNome() + "\nVida: " + w1.getVida() + "\tVida: " + w2.getVida() + "\n");
+         do{
            w1.ataca(w2);
            w2.ataca(w1);
-          } while(w1.isAlive() && w2.isAlive());
-    }
+         } while(w1.isAlive() && w2.isAlive());
    }
+}
    
--- Teste.java
+++ Teste.java
@@ -1,9 +1,10 @@
 public class Teste {
 	public static void main(String[] args){
+
 		Guerreiro warrior1 = new Guerreiro("Perseu", 80, 25);
-		Guerreiro warrior2 = new Guerreiro("Medusa",100, 10);
+		Guerreiro warrior2 = new Guerreiro("Medusa", 100, 10);
 		Luta luta = new Luta(warrior1,warrior2);
         
-        luta.batalha();
+        luta.inicio();
 	}
 }
