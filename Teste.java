c74712117b75c9d10accdd49533c9f3c797fe407
Gilvanilson Bernardino <gill.sbernardino@gmail.com>
2021-04-01 10:04:19 -0300

    Revert "Update Teste.java"
    This reverts commit b6b971c1bcc9f9dc95ca112c2a67a8120c42c0d0.

--- Teste.java
+++ Teste.java
@@ -4,22 +4,22 @@ public class Teste {
 	public static void main(String[] args){
 
 	    String nome1 = JOptionPane.showInputDialog("Digite o nome do seu Guerreiro: ");
-            String nome2 = JOptionPane.showInputDialog("Digite o nome do seu Guerreiro: ");
-            String nome3 = JOptionPane.showInputDialog("Digite o nome do seu Guerreiro: ");
-	    String nome4 = JOptionPane.showInputDialog("Digite o nome do seu Guerreiro: ");
+        String nome2 = JOptionPane.showInputDialog("Digite o nome do seu Guerreiro: ");
+        String nome3 = JOptionPane.showInputDialog("Digite o nome do seu Guerreiro: ");
+		String nome4 = JOptionPane.showInputDialog("Digite o nome do seu Guerreiro: ");
 
-	    Guerreiro warrior1 = new Guerreiro(nome1, 80, 25);
-	    Guerreiro warrior2 = new Guerreiro(nome2,100, 10);
-	    Guerreiro warrior3 = new Guerreiro(nome3, 90, 15);
-	    Guerreiro warrior4 = new Guerreiro(nome4,100, 22);
+		Guerreiro warrior1 = new Guerreiro(nome1, 80, 25);
+		Guerreiro warrior2 = new Guerreiro(nome2,100, 10);
+		Guerreiro warrior3 = new Guerreiro(nome3, 90, 15);
+		Guerreiro warrior4 = new Guerreiro(nome4,100, 22);
 
-	    Luta luta1 = new Luta(warrior1,warrior2);
-	    Luta luta2 = new Luta(warrior3,warrior4);
+		Luta luta1 = new Luta(warrior1,warrior2);
+		Luta luta2 = new Luta(warrior3,warrior4);
 		
-            Thread inicioLuta1 = new Thread(luta1);
-	    Thread inicioLuta2 = new Thread(luta2);
+        Thread inicioLuta1 = new Thread(luta1);
+		Thread inicioLuta2 = new Thread(luta2);
         
-	    inicioLuta1.start();
-	    inicioLuta2.start();
+		inicioLuta1.start();
+		inicioLuta2.start();
 	}
 }
